a7b6d0f83752c98c9b260516f3cc7067b4426a13
Kyle Theaker <kyle.theaker@yahoo.com>
2021-05-24 21:38:00 -0700

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
 
-def countdown
+
+def countdown(arg)
+    while arg > 0
+        puts "#{arg} SECOND(S)!"
+        arg -= 1
+    end
+
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(s)
+sleep 5
+end
+
